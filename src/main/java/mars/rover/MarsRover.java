3f4b5420252b29b389159ef5c85a6cca31b35db8
mahendra.gharge <mahendra.gharge@thoughtworks.com>
2022-06-08 11:22:15 +0530

    [Mahendra] REFACTOR. segrigate turn and move logic to respective functions

--- src/main/java/mars/rover/MarsRover.java
+++ src/main/java/mars/rover/MarsRover.java
@@ -2,39 +2,48 @@ package mars.rover;
 
 public class MarsRover {
 
+    private static String turnLeft(int x, int y, char direction, String instructions) {
+        if (direction == 'N') {
+            return move(x, y, 'W', instructions.substring(1, instructions.length()));
+        } else if (direction == 'W') {
+            return move(x, y, 'S', instructions.substring(1, instructions.length()));
+        } else if (direction == 'S') {
+            return move(x, y, 'E', instructions.substring(1, instructions.length()));
+        }
+        return move(x, y, 'N', instructions.substring(1, instructions.length()));
+    }
+
+    private static String turnRight(int x, int y, char direction, String instructions) {
+        if (direction == 'N') {
+            return move(x, y, 'E', instructions.substring(1, instructions.length()));
+        } else if (direction == 'W') {
+            return move(x, y, 'N', instructions.substring(1, instructions.length()));
+        } else if (direction == 'S') {
+            return move(x, y, 'W', instructions.substring(1, instructions.length()));
+        }
+        return move(x, y, 'S', instructions.substring(1, instructions.length()));
+    }
+
+    private static String moveOneStep(int x, int y, char direction, String instructions) {
+        if (direction == 'N') {
+            return move(x, y + 1, 'N', instructions.substring(1, instructions.length()));
+        } else if (direction == 'S') {
+            return move(x, y - 1, 'S', instructions.substring(1, instructions.length()));
+        } else if (direction == 'W') {
+            return move(x - 1, y, 'W', instructions.substring(1, instructions.length()));
+        }
+        return move(x + 1, y, 'E', instructions.substring(1, instructions.length()));
+    }
+
     public static String move(int x, int y, char direction, String instructions) {
         if (!instructions.isEmpty()) {
             char instruction = instructions.charAt(0);
             if (instruction == 'L') {
-                if (direction == 'N') {
-                    return move(x, y, 'W', instructions.substring(1, instructions.length()));
-                } else if (direction == 'W') {
-                    return move(x, y, 'S', instructions.substring(1, instructions.length()));
-                } else if (direction == 'S') {
-                    return move(x, y, 'E', instructions.substring(1, instructions.length()));
-                } else if (direction == 'E') {
-                    return move(x, y, 'N', instructions.substring(1, instructions.length()));
-                }
+                return turnLeft(x, y, direction, instructions);
             } else if (instruction == 'R') {
-                if (direction == 'N') {
-                    return move(x, y, 'E', instructions.substring(1, instructions.length()));
-                } else if (direction == 'W') {
-                    return move(x, y, 'N', instructions.substring(1, instructions.length()));
-                } else if (direction == 'S') {
-                    return move(x, y, 'W', instructions.substring(1, instructions.length()));
-                } else if (direction == 'E') {
-                    return move(x, y, 'S', instructions.substring(1, instructions.length()));
-                }
+                return turnRight(x, y, direction, instructions);
             } else if (instruction == 'M') {
-                if (direction == 'N') {
-                    return move(x, y + 1, 'N', instructions.substring(1, instructions.length()));
-                } else if (direction == 'S') {
-                    return move(x, y - 1, 'S', instructions.substring(1, instructions.length()));
-                } else if (direction == 'W') {
-                    return move(x - 1, y, 'W', instructions.substring(1, instructions.length()));
-                } else if (direction == 'E') {
-                    return move(x + 1, y, 'E', instructions.substring(1, instructions.length()));
-                }
+                return moveOneStep(x, y, direction, instructions);
             }
         }
         return x + " " + y + " " + direction;
